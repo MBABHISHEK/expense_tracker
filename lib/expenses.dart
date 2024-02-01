c83d49140e57e18031cfcdebe7084a44ad65d97c
ABHI <mba384420@gmail.com>
2024-02-01 21:08:50 +0530

    expenses_list added with list view builder

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'expenses_list.dart';
+import 'models/expense.dart';
 
 class Expenses extends StatefulWidget
 {
- const Expenses({super.key});
+  const Expenses({super.key});
 
  @override
   State<StatefulWidget> createState()
@@ -13,16 +15,34 @@ class Expenses extends StatefulWidget
 
 class _ExpensesState extends State<Expenses>
 {
+  final List<Expense> _registeredExpenses=[
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
  @override
   Widget build(BuildContext context)
  {
-   return const Scaffold(
-    body:Column(
-     children:
-      [
-      Text('The chart'),
-      Text('Expenses list...')
-     ],    )
+   return  Scaffold(
+       body:Column(
+         children: [
+           const Text('The Chart'),
+           Expanded(
+               child:
+               ExpensesList(
+                   expenses:_registeredExpenses)
+           )
+         ],
+       )
    );
  }
 }
