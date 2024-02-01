607e982bef46f9965e113bd79d6d90267c20e318
ABHI <mba384420@gmail.com>
2024-02-01 23:04:31 +0530

    appbar added

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -33,6 +33,15 @@ class _ExpensesState extends State<Expenses>
   Widget build(BuildContext context)
  {
    return  Scaffold(
+       appBar:AppBar(
+         title: const Text('Expense Tracker'),
+         actions: [
+           IconButton(
+               onPressed:(){},
+               icon:const Icon(Icons.add),
+           )
+         ],
+       ),
        body:Column(
          children: [
            const Text('The Chart'),
